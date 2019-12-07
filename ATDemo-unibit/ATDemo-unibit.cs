3a9233c07060865c5458269e0f116506ced6fc41
daqnanovkova <daqna_novkova@abv.bg>
2019-12-07 14:24:45 +0200

    ATDemo-unibit

---
+++ ATDemo-unibit/ATDemo-unibit.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace ATDemo_unibit
+{
+    [TestClass]
+    public class ATDemo
+    {
+        private IWebDriver driver;
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+        [TestMethod]
+        public void invalidmailorPass()
+        {
+            driver.Navigate().GoToUrl("https://www2.hm.com/bg_bg/index.html");
+            driver.Manage().Window.Size = new System.Drawing.Size(1366, 728);
+            driver.FindElement(By.LinkText("Вход")).Click();
+            {
+                var element = driver.FindElement(By.LinkText("Вход"));
+                Actions builder = new Actions(driver);
+                builder.MoveToElement(element).Perform();
+            }
+            {
+                var element = driver.FindElement(By.TagName("body"));
+                Actions builder = new Actions(driver);
+                builder.MoveToElement(element, 0, 0).Perform();
+            }
+            driver.FindElement(By.Id("modal-txt-signin-email")).Click();
+            driver.FindElement(By.Id("modal-txt-signin-email")).SendKeys("invalid_mail@gmail.com");
+            driver.FindElement(By.Id("modal-txt-signin-password")).Click();
+            driver.FindElement(By.Id("modal-txt-signin-password")).SendKeys("123456");
+            driver.FindElement(By.CssSelector(".js-set-session-storage")).Click();
+            System.Threading.Thread.Sleep(3000);
+            Assert.AreEqual(driver.FindElement(By.Id("modal-txt-signin-password-unknown-error-type-error")).Text,"Грешен имейл или парола, моля, опитайте отново.");
+        }
+        
+        }
+    }
+
---
+++ ATDemo-unibit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ATDemo-unibit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ATDemo-unibit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6994e357-862a-42db-97b2-4efe69fa67ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
